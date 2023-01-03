138ba828fbac4695a7e15822b4d752120e67a609
Sandaruwan Gamage <sandaruwan@ebuilder.local>
2023-01-03 17:00:12 +0530

    updated files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserServicesService } from '../user-services.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  title = 'Sandaruwan Gamage';
-  constructor(){ }
+export class HomeComponent implements OnInit{
 
-  ngOnInit()
+  data: Array<any> = [];
+  showSpinner = true;
+  title = 'Sandaruwan Gamage';
+  userData : any;
+
+  constructor(private userService: UserServicesService){}
+
+  ngOnInit():void
   {
+    this.getDetail();
     
+  }
+
+  getDetail() {
+    this.userService.getUserDetails().then((res) => {
+      res.json().then((data) => {
+      this.data = data;
+
+      console.log("HII");
+      console.log(data);
+
+        return data;
+
+      });
+
+    }).catch((err) => {
+
+      console.log(err)
+
+    });
   }
 }
--- src/app/user-services.service.ts
+++ src/app/user-services.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServicesService {
 
-  constructor() { }
+  constructor(
+    
+  ) { }
+
+  getUserDetails()
+  {
+    return fetch('https://fakestoreapi.com/users');
+  }
 }
