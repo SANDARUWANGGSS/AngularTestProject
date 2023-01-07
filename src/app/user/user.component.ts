12474f75c803c126161427d0e0051f20ec73ea92
Sandaruwan Gamage <sandaruwan@ebuilder.local>
2023-01-07 12:16:33 +0530

    Add data to user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+export interface Users {
+  id : string;
+  email : string;
+  username : string;
+  password : string;
+  firstname : string;
+  city : string;
+  // actions : string;
+}
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
+
+
+export class UserComponent implements OnInit{
+
+  userdetails: Users[] = [
+    {id : '1', email : 'sandaruwan@gmail.com', username : 'sandaruwan', password: 'dffcc', firstname: 'sandaruwan', city: 'walasmulla'  }
+  ];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
